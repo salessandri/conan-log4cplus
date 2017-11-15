7ecf43ecdf3ebbd5cc8e60aed7677a161fbbadad
Santiago Alessandri <san.lt.ss@gmail.com>
2017-11-15 14:07:32 -0800

    Remove the definition of _WIN32_WINNT

--- conanfile.py
+++ conanfile.py
@@ -25,6 +25,20 @@ index 3de896e5..2938b6a0 100644
  if (LOG4CPLUS_ENABLE_DECORATED_LIBRARY_NAME)
 '''
 
+SRC_CMAKELISTS_PATCH = '''diff --git a/src/CMakeLists.txt b/src/CMakeLists.txt
+index 82e52956..defc0831 100644
+--- a/src/CMakeLists.txt
++++ b/src/CMakeLists.txt
+@@ -71,7 +71,6 @@ if (UNICODE)
+ endif (UNICODE)
+ if (WIN32)
+   add_definitions (-DMINGW_HAS_SECURE_API=1)
+-  add_definitions (-D_WIN32_WINNT=${_WIN32_WINNT})
+
+   if (BUILD_SHARED_LIBS)
+     set(log4cplus_build_shared 1)
+'''
+
 class Log4cplusConan(ConanFile):
     name = "log4cplus"
     version = "1.2.0"
@@ -61,6 +75,7 @@ class Log4cplusConan(ConanFile):
         self.run("git clone https://github.com/log4cplus/log4cplus.git")
         self.run("cd log4cplus && git checkout REL_1_2_0")
         tools.patch('log4cplus', patch_string=CMAKELISTS_PATCH)
+        tools.patch('log4cplus', patch_string=SRC_CMAKELISTS_PATCH)
 
     def build(self):
         cmake = CMake(self)
